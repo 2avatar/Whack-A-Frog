dc9207f66190886bfc1c67296fe88d7f27771355
Eviatar Admon <eviataradmon@Eviatars-iMac.local>
2017-08-11 02:02:20 +0300

    day1v2

---
+++ Logic/Game.swift
@@ -0,0 +1,29 @@
+//
+//  Game.swift
+//  WhackAFrog
+//
+//  Created by Eviatar Admon on 8/10/17.
+//  Copyright © 2017 Eviatar Admon. All rights reserved.
+//
+
+import UIKit
+
+class Game: NSObject {
+    
+    var board: Board
+    var score: Int{
+        get{
+            return self.score
+        }
+        set (points){
+            self.score += points
+        }
+    }
+    
+    init (numOfTiles: Int){
+        //score = 0
+        board = Board(numOfTiles: numOfTiles)
+    }
+    
+    
+}
---
+++ Logic/Tile.swift
@@ -0,0 +1,29 @@
+//
+//  Tile.swift
+//  WhackAFrog
+//
+//  Created by Eviatar Admon on 8/10/17.
+//  Copyright © 2017 Eviatar Admon. All rights reserved.
+//
+
+import UIKit
+
+class Tile: NSObject {
+    
+    enum TileStates {
+        case Bad, Good, Empty
+    }
+    
+    var tileState: TileStates {
+        get{
+            return self.tileState
+        }
+        set (newState){
+            self.tileState = newState
+        }
+    }
+    
+    override init(){
+        //tileState = TileStates.Empty
+    }
+}
