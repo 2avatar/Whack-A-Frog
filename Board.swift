1c3da6f63004c8248d4cae04772896759f578734
Eviatar Admon <eviataradmon@Eviatars-iMac.local>
2017-08-11 01:25:44 +0300

    day1

---
+++ Board.swift
@@ -0,0 +1,58 @@
+//
+//  Board.swift
+//  WhackAFrog
+//
+//  Created by Eviatar Admon on 8/10/17.
+//  Copyright © 2017 Eviatar Admon. All rights reserved.
+//
+
+import UIKit
+
+class Board: NSObject {
+
+    var board: [Tile]
+    let numOfTiles: Int
+    let minTileTime = 2
+    let maxTileTime = 4
+    let minNumberOfTiles = 0
+    let maxNumberOfTiles = 4
+    
+    init (numOfTiles: Int){
+        board = [Tile]()
+        self.numOfTiles = numOfTiles
+        for _ in 0 ..< self.numOfTiles{
+            board.append(Tile())
+        }
+    }
+    
+    func setTileStateByPosition(pos: Int, state: Tile.TileStates){
+        board[pos].tileState = state
+    }
+    
+    func getTileStateByPosition(pos: Int) -> Tile.TileStates{
+    return board[pos].tileState
+    }
+    
+    func roleRandomNumberOfTiles() -> Int{
+        return randomInt(min: minNumberOfTiles, max: maxNumberOfTiles)
+    }
+    
+    func roleRandomTimeForTile() -> Int{
+       return randomInt(min: minTileTime, max: maxTileTime)
+    }
+    
+    func roleRandomTargetTile() -> Int{
+    
+        var target = randomInt(min: 0, max: numOfTiles)
+        while board[target].tileState != Tile.TileStates.Empty{
+            target = randomInt(min: 0, max: numOfTiles)
+        }
+        
+        return target
+    }
+    
+    func randomInt(min: Int, max: Int) -> Int {
+        return min + Int(arc4random_uniform(UInt32(max - min + 1 )))
+    }
+    
+}
---
+++ WhackAFrog/CollectionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CollectionViewController.swift
+//  WhackAFrog
+//
+//  Created by Eviatar Admon on 8/10/17.
+//  Copyright © 2017 Eviatar Admon. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
+        
+        
+        let button = cell.viewWithTag(1) as! UIButton
+        
+        
+        button.setBackgroundImage(UIImage(named: "Images/frog.jpg"), for: UIControlState.normal)
+        
+        return cell
+    }
+}
+
